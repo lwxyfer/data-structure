6cb2da7c76a02287badccf92acc6444bf647db16
lwxyfer <lwxyfer@gmail.com>
2017-08-10 23:11:50 +0800

    add set

--- index.js
+++ index.js
@@ -188,3 +188,76 @@ class LinkedList {
     return this.length;
   }
 }
+
+
+/**
+ * Set
+ */
+class Set {
+  constructor() {
+    this.items = {}
+  }
+
+  has(value) {
+    return this.items.hasOwnProperty(value)
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.items[value] = value
+      return true
+    }
+    return false
+  }
+
+  remove(value) {
+    if (this.has(value)) {
+      delete this.items[value]
+      return true
+    }
+    return false
+  }
+
+  get size() {
+    return Object.keys(this.items).length
+  }
+
+  get values() {
+    return Object.keys(this.items)
+  }
+
+  union(otherSet) {
+    const unionSet = new Set()
+    this.values.forEach((v, i) => unionSet.add(this.values[i]))
+    otherSet.values.forEach((v, i) => unionSet.add(otherSet.values[i]))
+    return unionSet
+  }
+
+  intersection(otherSet) {
+    const intersectionSet = new Set()
+    this.values.forEach((v, i) => {
+      if (otherSet.has(v)) {
+        intersectionSet.add(v)
+      }
+    })
+    return intersectionSet
+  }
+
+  difference(otherSet) {
+    const differenceSet = new Set()
+    this.values.forEach((v, i) => {
+      if (!otherSet.has(v)) {
+        differenceSet.add(v)
+      }
+    })
+    return differenceSet
+  }
+
+  subset(otherSet) {
+    if (this.size > otherSet.size) {
+      return false
+    } else {
+      return !this.values.some(v => !otherSet.has(v))
+    }
+  }
+}
