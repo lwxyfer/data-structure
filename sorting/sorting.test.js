2a6bc3e40bb02db587140faca9d80b4c8ea1b9df
lwxyfer <lwxyfer@gmail.com>
2017-08-13 22:25:58 +0800

    optimize test

--- sorting/sorting.test.js
+++ sorting/sorting.test.js
@@ -5,27 +5,31 @@ import {
   mergeSortBU
 } from './sorting.js';
 
+const originArray = [3, 65, -23, 14, 55, 14, 34];
+const ascendArray = [-23, 3, 14, 14, 34, 55, 65];
+const decentArray = [65, 55, 34, 14, 14, 3, -23]
+
 // selection
 test('[3,65,-23,14,55,14,34] ascending order is [3, 14, 23, 34, 43, 55, 65]', () => {
-  expect(selection([3,65,-23,14,55,14,34])).toEqual([-23, 3, 14, 14, 34, 55, 65]);
+  expect(selection(originArray)).toEqual(ascendArray);
 });
 
 test('[3,65,-23,14,55,14,34] descending order is [65, 55, 43, 34, 23, 14, 3]', () => {
-  expect(selection([3,65,-23,14,55,14,34], true)).toEqual([65, 55, 34, 14, 14, 3, -23]);
+  expect(selection(originArray, true)).toEqual(decentArray);
 });
 
 // insertion
 test('[3,65,-23,14,55,14,34] ascending order is [3, 14, 23, 34, 43, 55, 65]', () => {
-  expect(insertion([3,65,-23,14,55,14,34])).toEqual([-23, 3, 14, 14, 34, 55, 65]);
+  expect(insertion(originArray)).toEqual(ascendArray);
 });
 
 test('[3,65,-23,14,55,14,34] ascending order is [3, 14, 23, 34, 43, 55, 65]', () => {
-  expect(insertion([3,65,-23,14,55,14,34], true)).toEqual([65, 55, 34, 14, 14, 3, -23]);
+  expect(insertion(originArray, true)).toEqual(decentArray);
 });
 
 // merge
 test('[3,65,-23,14,55,14,34] ascending order is [3, 14, 23, 34, 43, 55, 65]', () => {
-  expect(mergeSort([3,65,-23,14,55,14,34])).toEqual([-23, 3, 14, 14, 34, 55, 65]);
+  expect(mergeSort(originArray)).toEqual(ascendArray);
 });
 
 test('[3,65,-23,14,55,14,34] ascending order is [3, 14, 23, 34, 43, 55, 65]', () => {
